ee6cc3b674e2cfb1f9c78622dcbf77e1ef91759d
acer <acer@DESKTOP-H9LFNLB.domain.name>
2020-04-03 12:53:38 +0545

    Updated SubTeamController , produces = MediaType.APPLICATION_JSON_VALUE

--- src/main/java/com/ftth/controller/SubTeamController.java
+++ src/main/java/com/ftth/controller/SubTeamController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,7 +48,7 @@ public class SubTeamController {
 	
 	
 	@ResponseBody
-    @RequestMapping(value = "/subteam/jsonlist", method = RequestMethod.GET)
+    @RequestMapping(value = "/subteam/jsonlist", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Map<String, Object>> getSPtargetist(Locale locale, Model model, HttpSession session)
             throws SQLException {
 		SubTeamDao dao = new SubTeamDao();
